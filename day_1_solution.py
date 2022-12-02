85ad79f6c8d7709523440c5cf7f61694bff0465c
unknown <12560602+mauritz2@users.noreply.github.com>
2022-12-02 17:41:49 +0100

    Day 1 part 2 solution

--- day_1_solution.py
+++ day_1_solution.py
@@ -1,13 +1,28 @@
-# Validated as correct solution
-with open("day_1_data.txt", "r") as f:
-    calorie_sum = []
-    data = f.readlines()
+# GET DATA
+f = open("day_1_data.txt", "r")
+data = f.readlines()
+f.close()
+
+# PART 1
+# Validated answer as correct
+
+def get_calories_by_elf(calories):
+    calories_sum_by_elf = []
     current_elf_sum = 0
-    for line in data:
+    for line in calories:
         if line == "\n":
-            calorie_sum.append(current_elf_sum)
+            calories_sum_by_elf.append(current_elf_sum)
             current_elf_sum = 0
         else:
             current_elf_sum += int(line)
-    print(max(calorie_sum))
+    return calories_sum_by_elf
 
+calories_by_elf = get_calories_by_elf(data)
+print(max(calories_by_elf))
+
+# PART 2
+# Validated answer as correct
+
+calories_by_elf.sort(reverse=True)
+top_three = calories_by_elf[:3]
+print(sum(top_three))

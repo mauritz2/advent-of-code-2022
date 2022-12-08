260f7550a4e3f451719b649a02307afdaf0ccc96
unknown <12560602+mauritz2@users.noreply.github.com>
2022-12-08 11:32:26 +0100

    WIP day 8

--- day_8_solution.py
+++ day_8_solution.py
@@ -5,33 +5,56 @@ data = f.readlines()
 data = list(map(lambda x: x.strip(), data))
 f.close()
 
-def is_visible(tree_val: int, tree_row: int, tree_col: int, trees: list[int]) -> bool:
-    # Check up
 
+def is_visible_in_one_direction(trees:list[str], variable_axis_start: list[str], range_direction:int, variable_axis:str, fixed_axis_val:str, tree_val:int):
     visible = False
 
+    range_end = -1 if range_direction == -1 else max(variable_axis_start)
+
+    #for index in range(variable_axis_start, range_end, range_direction):
+    for index in range(variable_axis_start, range_end, range_direction):
+
+        if variable_axis == "row":
+            comparison_tree_val= int(trees[index][fixed_axis_val])
+        elif variable_axis == "col":
+            comparison_tree_val= int(trees[fixed_axis_val][index])
+      
+        print(f"Comparing: {comparison_tree_val} to {tree_val}")
+        if comparison_tree_val >= tree_val:
+            # Tree is not visible from this direction - 
+            print("Breaking! Not visible")
+            break
+    else:
+        print("Tree is visible")
+        visible = True
+    
+    return visible
+
+
+def is_visible(tree_val: int, tree_row: int, tree_col: int, trees: list[int]) -> bool:
     print(f"\n\nLooking into ({tree_row}, {tree_col}): {tree_val}")
 
-    first_row_above = tree_row - 1
-    if first_row_above >= 0:
-        # We need -1 instead of 0 because the end is non-inclusive
-        for row_i in range(first_row_above, -1, -1):
-            comparison_tree_val= int(trees[row_i][tree_col])
-            print(f"Comparing: {comparison_tree_val} to {tree_val}")
-            if comparison_tree_val >= tree_val:
-                # Tree is not visible from this direction - 
-                print("Breaking! Not visible")
-                break
-        else:
-            print("This tree is visible")
-            # Couldn't find a single tree in this direction that was taller
-            visible = True
-    else:
-        # This is an edge tree
-        print("This is an edge upwards so its visible")
-        visible = True
+    # Check if edge node 
+    if ((tree_row == 0) or (tree_row == len(trees)) or (tree_col == 0) or (tree_col == len(trees[0]))):
+        print("This is an edge node. It's visible")
+        return True
 
-    return visible
+    # Check up
+    variable_axis_start = tree_row - 1
+    range_direction = -1
+    variable_axis = "row"
+    fixed_axis_val = tree_col
+    tree_val = tree_val
+
+    visible_up = is_visible_in_one_direction(
+        trees=trees,
+        variable_axis_start=variable_axis_start,
+        range_direction=range_direction,
+        variable_axis=variable_axis,
+        fixed_axis_val=fixed_axis_val,
+        tree_val=tree_val)
+    
+    
 
     # Check down
 
